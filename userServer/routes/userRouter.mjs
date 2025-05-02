7606d8be02c63b9b4f286fe5197a4d951b352e87
Azaken1248 <suvkrm@gmail.com>
2025-05-03 01:01:05 +0530

    Updated Routes And Testers

--- userServer/routes/userRouter.mjs
+++ userServer/routes/userRouter.mjs
@@ -1,13 +1,16 @@
 import express from "express";
+import { authenticateJWT } from "../middleware/authMiddleware.mjs";
 import {
   createUserHandler,
   getAllUsersHandler,
   getUserHandler,
-  loginUserHandler
+  loginUserHandler,
+  requestTransactionHandler
 } from "../controllers/userController.mjs";
 
 const userRouter = express.Router();
 
+userRouter.post('/request', authenticateJWT, requestTransactionHandler);
 userRouter.post("/signup", createUserHandler);
 userRouter.post("/login", loginUserHandler);
 userRouter.get("/:username", getUserHandler);
